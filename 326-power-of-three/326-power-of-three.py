759398165f2604fb0c755773c223e9c884e2ba80
Natan Mekebib <83671649+nati589@users.noreply.github.com>
2023-02-01 10:58:44 -0900

    Time: 111 ms (41.47%), Space: 13.9 MB (13.74%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.py
@@ -0,0 +1,11 @@
+class Solution:
+    def recurse(self, n):
+        return n / 3
+    def isPowerOfThree(self, n: int) -> bool:
+        while n > 1:
+            n = self.recurse(n)
+        if n == 1:
+            return True
+        else: 
+            return False    
+        
