190c19215ceb1dddb1fdc1adfe8daebada1eb154
Natan Mekebib <83671649+nati589@users.noreply.github.com>
2023-10-26 14:18:07 +0300

    Time: 64 ms (13.21%), Space: 16.9 MB (31.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,12 +3,13 @@ class Solution:
         check1 = {}
         check2 = {}
         
+        if len(s) != len(t):
+            return False
         for i in range(len(s)):
             if s[i] in check1.keys():
                 check1[s[i]] += 1
             else: 
                 check1[s[i]] = 0
-        for i in range(len(t)):
             if t[i] in check2.keys():
                 check2[t[i]] += 1
             else: 
