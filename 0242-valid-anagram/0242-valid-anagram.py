770f888dc4d1234471fb59ad4e2542b0e4f27bb8
Natan Mekebib <83671649+nati589@users.noreply.github.com>
2023-10-26 14:13:50 +0300

    Time: 66 ms (9.66%), Space: 16.9 MB (44.80%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        check1 = {}
+        check2 = {}
+        
+        for i in range(len(s)):
+            if s[i] in check1.keys():
+                check1[s[i]] += 1
+            else: 
+                check1[s[i]] = 0
+        for i in range(len(t)):
+            if t[i] in check2.keys():
+                check2[t[i]] += 1
+            else: 
+                check2[t[i]] = 0
+        if check1 == check2:
+            return True
+        else: 
+            return False
+        
